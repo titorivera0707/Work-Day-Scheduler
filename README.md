bc3ea7b80d7a5155899e417bf7cd9b85f3d1fb1b
Tito Rivera <titorivera0707@gmail.com>
2020-11-08 21:43:09 -0800

    troubleshooting

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Work-Day-Scheduler
+### Work-Day-Scheduler
+
+### Motivation
+
+* The motivation behind making this work day scheduler is to help people keep track of everything going on during their day. Some people have a very busy schedule during the day whether at work or not. This work day scheduler allows you to schedule everything that you need to do during the day hour by hour. Getting distracted during a typical day is almost an inevitability. You can use different means of keeping track of day to day activites such as a journal or calendar. The only problem with those methods is that if you forget them at home or at the office, you've already lost everything that you had written down until you find it again. The difference with this workday scheduler is that it can be accessed by mobile phone and no one ever forgets their phone.
+
+### Features
+
+* This application, in it's current build, allows you to save things that you need to do by the hour. It allows you to see what time it is and what time you're next event/meeting will be. This app shows your what time it currently is by highlighting the center box in red. It also shows what time already passed in gray and what time it will be soon in green.
+
+### Build Status
+
+* This build is close to being completely finished. Right now it is currently still a very good tool to show what time it is now and what time your next meetings/events will be. At the end of the build, server storage may be added to help you save information of your day to day activities across different platforms.
+
+### Screenshots
+
+*![Basic Screen](.)
--- script.js
+++ script.js
@@ -13,17 +13,92 @@ document.getElementById("currentDay").innerHTML = d + ", " + m + " " + dt
 
 function initial() {
     for (var i = 0; i < workHours.length; i++) {
-      $('.container').append("<div class='row'><div id='time-block' class='time-block col-2'><h3 class='hour'>" + workHours[i] + "</h3></div><textarea class='description col-8'></textarea><button class='saveBtn Btn" + i + " col-2'>Save</button></<div>");
+      $('.container').append("<div class='row'><div id='time-block' class='time-block col-2'><h3 class='hour hr" + i + "'>" + workHours[i] + "</h3></div><textarea class='description dscrptn" + i + " col-8'></textarea><button class='saveBtn Btn" + i + " col-2'>Save</button></<div>");
     }
         var description = document.querySelector(".description")
         var hour = document.querySelector(".hour")
+        if (JSON.parse(localStorage.getItem(description.value))){
+            $description.textContent(JSON.parse(localStorage.getItem(description.value)))
+        }
+        if (hour === "9"){
+            $(".dscrptn0").addClass("present")
+        }
+        if (hour < "9"){
+            $(".dscrptn0").addClass("future")
+        }else{
+            $(".dscrptn0").addClass("past")
+        }
+        if (hour === "10"){
+            $(".dscrptn1").addClass("present")
+        }
+        if (hour < "10"){
+            $(".dscrptn1").addClass("future")
+        }else{
+            $(".dscrptn1").addClass("past")
+        }
+        if (hour === "11"){
+            $(".dscrptn2").addClass("present")
+        }
+        if (hour < "11"){
+            $(".dscrptn2").addClass("future")
+        }else{
+            $(".dscrptn2").addClass("past")
+        }
+        if (hour === "12"){
+            $(".dscrptn3").addClass("present")
+        }
+        if (hour < "12"){
+            $(".dscrptn3").addClass("future")
+        }else{
+            $(".dscrptn3").addClass("past")
+        }
+        if (hour === "13"){
+            $(".dscrptn4").addClass("present")
+        }
+        if (hour < "13"){
+            $(".dscrptn4").addClass("future")
+        }else{
+            $(".dscrptn4").addClass("past")
+        }
+        if (hour === "14"){
+            $(".dscrptn5").addClass("present")
+        }
+        if (hour < "14"){
+            $(".dscrptn5").addClass("future")
+        }else{
+            $(".dscrptn5").addClass("past")
+        }
+        if (hour === "15"){
+            $(".dscrptn6").addClass("present")
+        }
+        if (hour < "15"){
+            $(".dscrptn6").addClass("future")
+        }else{
+            $(".dscrptn6").addClass("past")
+        }
+        if (hour === "16"){
+            $(".dscrptn7").addClass("present")
+        }
+        if (hour < "16"){
+            $(".dscrptn7").addClass("future")
+        }else{
+            $(".dscrptn7").addClass("past")
+        }
+        if (hour === "17"){
+            $(".dscrptn8").addClass("present")
+        }
+        if (hour < "17"){
+            $(".dscrptn8").addClass("future")
+        }else{
+            $(".dscrptn8").addClass("past")
+        }
         $(".saveBtn").on("click", function()   {
-        localStorage.setItem(hour.value, JSON.stringify(description.value))
+        localStorage.setItem(JSON.stringify(hour.textContent), JSON.stringify(description.value))
         console.log(description.value);
         console.log(hour.textContent)
-        console.log("Btn0")
+        console.log(typeof(hour))
 });
-localStorage.getItem(workHours[i])
+
 }
 var description = JSON.parse(localStorage.getItem("hello"))
 initial()
