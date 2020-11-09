ab60083212103b6e228b954ab1b315edbaf6926c
Tito Rivera <titorivera0707@gmail.com>
2020-11-08 17:48:59 -0800

    troubleshooting

--- script.js
+++ script.js
@@ -10,25 +10,26 @@ var m = (months[month]);
 var dt = date
 workHours = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
 document.getElementById("currentDay").innerHTML = d + ", " + m + " " + dt 
+
 function initial(){
     for (var i = 0; i < 9; i++) {
         $(".container").append("<div class='row'></<div>");
     }  
-        $(".row").append("<div id='time-block' class='time-block col-2'><textarea class='userText'></textarea></div>")
+        $(".row").append("<div id='time-block' class='time-block col-2'><textarea class='userText col-12'></textarea></div>")
         $(".time-block").append("<h3 class='hour'></h3>")
         $(".row").append ("<textarea class='description col-8'></textarea>")
         $(".row").append("<button class='saveBtn col-2'></button>")
-        // document.querySelector(".hour").innerHTML = "hello"
-        var description = document.querySelector(".description")
+    }
+
+function next(){
+    var description = document.querySelector(".description")
         var hour = document.querySelector(".hour")
         var userText = document.querySelector(".userText")
         $(".saveBtn").on("click", function()   {
             localStorage.setItem(userText.value, JSON.stringify(description.value))
             console.log(description.value);
             console.log(hour.textContent)
-
-    })
+})
 }
-
 var description = JSON.parse(localStorage.getItem("hello"))
 initial()
